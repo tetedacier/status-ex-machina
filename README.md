e5f6e0c5f72c424bf3fabb40e5df7d0fb92372ba
Fabien Jacq <f.jacq@testamento.fr>
2022-11-18 23:06:09 +0100

    Initialize project

---
+++ README.md
@@ -0,0 +1,14 @@
+# status-ex-machina
+
+> Set up manifests here
+
+## Milestone
+### xState react api usage
+
+> https://xstate.js.org/docs/guides/communication.html
+## References
+
+> Set up references here here
+
+
+[How to use this application](./docs/app.md)
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Writer from './Writer';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* <Writer /> */}
+            </header>
+        </div>
+    );
+}
+
+export default App;
